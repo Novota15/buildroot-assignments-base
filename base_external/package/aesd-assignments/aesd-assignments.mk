7a16639cab4019a3a52f86d2d0b14738ce4f2286
Grant Novota <47872713+Novota15@users.noreply.github.com>
2025-10-27 19:36:22 -0600

    Update aesd-assignments.mk

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 78f2039
+AESD_ASSIGNMENTS_VERSION = 4f4cda925e8636f68ed81d2743ec1d20c0ba7f7d
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
